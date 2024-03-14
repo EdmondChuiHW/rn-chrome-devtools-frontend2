48fef4bc8f515f1590c07c8e808f606cec9ad3d7
Edmond Chui <1967998+EdmondChuiHW@users.noreply.github.com>
2024-03-14 17:02:08 +0000

    add breakpoint events

--- front_end/core/host/RNPerfMetrics.ts
+++ front_end/core/host/RNPerfMetrics.ts
@@ -14,6 +14,10 @@ export function getInstance(): RNPerfMetrics {
   return instance;
 }
 
+function getPerfTimestamp(): DOMHighResTimeStamp {
+  return performance.timeOrigin + performance.now();
+}
+
 type UnsunscribeFn = () => void;
 class RNPerfMetrics {
   #listeners: Set<RNReliabilityEventListener> = new Set();
@@ -51,6 +55,38 @@ class RNPerfMetrics {
       console.error('Error occurred when calling event listeners', error);
     }
   }
+
+  setBreakpointRequest(params: SetBreakpointRequestEvent['params']): void {
+    this.sendEvent({
+      eventName: 'Debugger.SetBreakpoint.Request',
+      timestamp: getPerfTimestamp(),
+      params,
+    });
+  }
+
+  setBreakpointResponse(params: SetBreakpointResponseEvent['params']): void {
+    this.sendEvent({
+      eventName: 'Debugger.SetBreakpoint.Response',
+      timestamp: getPerfTimestamp(),
+      params,
+    });
+  }
+
+  breakpointResolved(params: BreakpointResolvedEvent['params']): void {
+    this.sendEvent({
+      eventName: 'Debugger.BreakpointResolved',
+      timestamp: getPerfTimestamp(),
+      params,
+    });
+  }
+
+  debuggerPaused(params: DebuggerPausedEvent['params']): void {
+    this.sendEvent({
+      eventName: 'Debugger.Paused',
+      timestamp: getPerfTimestamp(),
+      params,
+    });
+  }
 }
 
 export function registerPerfMetricsGlobalPostMessageHandler(): void {
@@ -64,4 +100,53 @@ export function registerPerfMetricsGlobalPostMessageHandler(): void {
   });
 }
 
-export type ReactNativeChromeDevToolsEvent = {};
+export type SetBreakpointEventEntryPoint =|'fileGutterClicked'|'savedStateRestored';
+export type SetBreakpointEventType =|'logpoint'|'unconditionalBreakpoint'|'conditionalBreakpoint';
+
+export type BreakpointLocation = Readonly<{
+  scriptId: string,
+  lineNumber: number,
+  columnNumber: number,
+}>;
+
+export type SetBreakpointRequestEvent = Readonly<{
+  eventName: 'Debugger.SetBreakpoint.Request',
+  timestamp: DOMHighResTimeStamp,
+  params: Readonly<{
+    entryPoint: SetBreakpointEventEntryPoint,
+    requestID: string,
+    type: SetBreakpointEventType,
+    requestedLocation: BreakpointLocation,
+  }>,
+}>;
+
+export type SetBreakpointResponseEvent = Readonly<{
+  eventName: 'Debugger.SetBreakpoint.Response',
+  timestamp: DOMHighResTimeStamp,
+  params: Readonly<{
+    breakpointID: string,
+    requestID: string,
+    actualLocation: BreakpointLocation,
+  }>,
+}>;
+
+export type BreakpointResolvedEvent = Readonly<{
+  eventName: 'Debugger.BreakpointResolved',
+  timestamp: DOMHighResTimeStamp,
+  params: Readonly<{
+    breakpointID: string,
+    actualLocation: BreakpointLocation,
+  }>,
+}>;
+
+export type DebuggerPausedEvent = Readonly<{
+  eventName: 'Debugger.Paused',
+  timestamp: DOMHighResTimeStamp,
+  params: Readonly<{
+    hitBreakpointIDs: Readonly<string[]>| null | void,
+    location: BreakpointLocation,
+  }>,
+}>;
+
+type ReactNativeChromeDevToolsEvent =
+    |SetBreakpointRequestEvent|SetBreakpointResponseEvent|BreakpointResolvedEvent|DebuggerPausedEvent;
